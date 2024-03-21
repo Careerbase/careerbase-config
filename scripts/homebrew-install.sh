f81ece2b6aa93b1ad0691633b1d2f165880f4227
Tom West <55838419+twestos@users.noreply.github.com>
2024-03-21 16:06:23 +1100

    Update homebrew-install.sh

--- scripts/homebrew-install.sh
+++ scripts/homebrew-install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-echo HELLO!
-
 # Install Xcode command line tools
-#xcode-select --install
+xcode-select --install
 
 # Install homebrew
-#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
