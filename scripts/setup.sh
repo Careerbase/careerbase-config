3fc1d3b86e12f0d9aa722347f2e5c25dba38e553
Tom West <55838419+twestos@users.noreply.github.com>
2024-03-21 17:02:44 +1100

    more changes

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,18 +8,21 @@ xcode-select --install
 
 # Append the Homebrew environment setup script to the user's .zprofile
 # Using the $HOME environment variable to get the user's home directory
-echo -e "\neval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> "$HOME/.zprofile"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> $HOME/.zprofile
 
 # Evaluate the Homebrew environment setup script to update the current session
-eval "$($(brew --prefix)/bin/brew shellenv)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Create config directory
+mkdir ~/.config
 
 # Install deps
-brew install node pnpm tmux pscale mysql-client
-
-# Install ngrok
+brew install node 
+brew install pnpm
+brew install tmux
+brew install planetscale/tap/pscale
+brew install mysql-client
 brew install --cask ngrok
-
-# Install iTerm2
 brew install --cask iterm2
 
 # Install global node module
